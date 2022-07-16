5a9ee8b736617f939c40c1553ea5e57fc7c25989
vallenov <a2lenov@mail.ru>
2022-07-17 02:16:32 +0300

    Исправлено удаление элемента + добавлен подсчет уровней дерева + добавлен новый метод отрисовки (требуется небольшая доработка)

--- pytrees/trees/base_tree.py
+++ pytrees/trees/base_tree.py
@@ -16,13 +16,29 @@ class BaseNode:
 class BinaryTree:
     def __init__(self):
         self.head_node = None
-        self.level_count = 1
 
     def print_tree(self):
         if not self.head_node:
             print('Tree is empty')
             return
-        self._passing(self.head_node)
+        self._passing_draw()
+        print(f'Levels count: {self.levels_count()}')
+
+    def levels_count(self):
+        level_count = 0
+        if not self.head_node:
+            return level_count
+        lst = [self.head_node]
+        while lst:
+            level_count += 1
+            tmp = lst.copy()
+            lst = []
+            for node in tmp:
+                if node.left_edge:
+                    lst.append(node.left_edge)
+                if node.right_edge:
+                    lst.append(node.right_edge)
+        return level_count
 
     def _passing(self, current_node: BaseNode):
         if not current_node:
@@ -31,24 +47,44 @@ class BinaryTree:
         self._passing(current_node.left_edge)
         self._passing(current_node.right_edge)
 
+    def _passing_draw(self):
+        lst = [self.head_node]
+        lev_tree = self.levels_count()
+        cnt_in_lst = 1
+        while lst:
+            string = ''
+            for node in lst:
+                interval = lev_tree * 3
+                cnt_in_lst *= 2
+                string += ' ' * interval + f'{" " if not node else node.value}'
+            print(string)
+            lev_tree -= 1
+            tmp = lst.copy()
+            lst = []
+            for node in tmp:
+                if not node:
+                    continue
+                if node.left_edge:
+                    lst.append(node.left_edge)
+                else:
+                    lst.append(None)
+                if node.right_edge:
+                    lst.append(node.right_edge)
+                else:
+                    lst.append(None)
+
     def _search(self, current_node: BaseNode, value: int):
         if not current_node:
-            print('None')
             return
         if current_node.value != value:
             if current_node.left_edge:
-                print('left')
                 self._search(current_node.left_edge, value)
             elif current_node.right_edge:
-                print('right')
                 self._search(current_node.right_edge, value)
-            else:
-                print('None None')
-                #current_node.left_edge = BaseNode(value)
-                return
+            return
         else:
-            print('res', current_node)
-            return current_node
+            print(current_node.value)
+            return current_node.value
 
     def index(self, value: int):
         return self._search(self.head_node, value)
@@ -78,22 +114,27 @@ class BinaryTree:
             if current_node.left_edge.left_edge:
                 current_node.left_edge.value = current_node.left_edge.left_edge.value
                 current_node.left_edge.left_edge.right_edge = current_node.left_edge.right_edge
+                current_node.left_edge.left_edge = None
             elif current_node.left_edge.right_edge:
+                current_node.left_edge.right_edge.left_edge = current_node.left_edge.left_edge
                 current_node.left_edge = current_node.left_edge.right_edge
-                del current_node.left_edge.right_edge
-                return
+            else:
+                current_node.left_edge = None
+            return
         self._remove_recursive(current_node.left_edge, value)
         if not current_node.right_edge:
             return
         if current_node.right_edge.value == value:
             if current_node.right_edge.left_edge:
+                current_node.right_edge.left_edge.right_edge = current_node.right_edge.right_edge
                 current_node.right_edge = current_node.right_edge.left_edge
-                del current_node.right_edge.left_edge
             elif current_node.right_edge.right_edge:
                 current_node.right_edge.value = current_node.right_edge.right_edge.value
                 current_node.right_edge.right_edge.left_edge = current_node.right_edge.left_edge
-                del current_node.right_edge.left_edge
-                return
+                current_node.right_edge.right_edge = None
+            else:
+                current_node.right_edge = None
+            return
         self._remove_recursive(current_node.right_edge, value)
 
     def _append_recursive(self, current_node: BaseNode, value: int):
