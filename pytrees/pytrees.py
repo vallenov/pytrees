451b283f406560ede851e0de8474b03294f1ffce
vallenov <a2lenov@mail.ru>
2023-12-18 00:43:06 +0300

    -

--- pytrees/pytrees.py
+++ pytrees/pytrees.py
@@ -1,9 +1,29 @@
-from trees.base_tree import BaseNode, BinaryTree
+import random
+
+from trees.base_tree import BinaryTree
 
 
 if __name__ == '__main__':
     tr = BinaryTree()
-    tr.append(5)
-    tr.append(3)
+    tr.extend([5, 3, 7, 12, 0, 7, 15])
+    tr.print_tree()
+    tr.levels_count()
+    tr.remove(8)
+    tr.print_tree()
+    tr.remove(7)
+    tr.print_tree()
     tr.append(7)
+    tr.print_tree()
+    print('---------')
+    print(tr.index(20))
+    print(tr.index(7))
+    print(1 in tr)
+    print('---------')
+    tr.clear()
+    l = list(range(10))
+    random.shuffle(l)
+    tr.extend(l)
+    tr.print_tree()
+    tr.clear()
+    tr.extend(['qwe', 'rfd', 'tvf', 'cvb', 'zxc'])
     tr.print_tree()
